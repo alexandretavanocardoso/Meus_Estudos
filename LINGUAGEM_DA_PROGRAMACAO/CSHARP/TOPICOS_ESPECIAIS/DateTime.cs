3053f93192f6003630ef45248e57199101bd03ed
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2020-12-29 13:42:47 -0300

    Update

--- LINGUAGEM_DA_PROGRAMACAO/CSHARP/TOPICOS_ESPECIAIS/DateTime.cs
+++ LINGUAGEM_DA_PROGRAMACAO/CSHARP/TOPICOS_ESPECIAIS/DateTime.cs
@@ -1,2 +1,183 @@
 
 /* DATETIME */
+-- Representa um instante
+
+-- dd/MM/yyyy - dia/mes/ano
+-- yyyy-MM-dd - ano-mes-dia
+-- HH:mm:ss - Hora:Minuto:Segundo
+-- HH:mm:ss..fff - Hora:Minuto:Segundo:Milisegundo
+
+----- Instanciando -----
+
+Datetime d1 = DateTime.Now; // Horario Atual
+
+----- Instanciando Construtor -----
+
+Datetime d1 = new DateTime(ano, mes, dia);
+Datetime d2 = new DateTime(ano, mes, dia, hora, minuto, segundo);
+Datetime d3 = new DateTime(ano, mes, dia, hora, minuto, segundo, milisegundo);
+
+----- Builders Básicos -----
+
+Datetime d1 = DateTime.Now; // Data e Horario Atual
+Datetime d2 = DateTime.Today; // Data Atual
+Datetime d3 = DateTime.utcNow; // Horario de agora, porem universal
+
+----- Usando Parse -----
+- Converte string em data
+
+Datetime d1 = Datetime.Parse("ano-mes-dia"); // Converte string em data
+Datetime d2 = Datetime.Parse("ano-mes-dia hora:minuto:segundo"); // Converte string em data
+
+Datetime d3 = Datetime.Parse("dia/mes/ano"); // Converte string em data
+Datetime d4 = Datetime.Parse("dia/mes/ano hora:minuto:segundo"); // Converte string em data
+
+----- Usando ParseExact -----
+- O proprio autor formata a data
+
+Datetime d1 = Datetime.ParseExact("ano-mes-dia", "Mascara de Formatação");
+
+Ex.
+Datetime d1 = Datetime.ParseExact("2002-08-28", "yyyy-MM-dd", cultureInfo.InvariantCulture);
+Datetime d2 = Datetime.ParseExact("15/08/2000 13:05:20", "dd/MM/yyyy HH:mm:ss", cultureInfo.InvariantCulture);
+
+----- Propriedades -----
+
+DateTime d = new DateTime(2000, 8, 12, 13, 45, 58, 275);
+
+-- Date (DateTime) --
+
+console.WriteLine(d.Date); // (2000, 8, 12
+
+-- Day (Int) --
+
+console.WriteLine(d.Day); // 12
+
+-- DayOfWeek (DayOfWeek --
+
+console.WriteLine(d.DayOfWeek); // Dia da Semana 
+
+-- DayOfYear (DayOfYear) --
+
+console.WriteLine(d.DayOfWeek); // Dia do ano 
+
+-- Hour (Int) --
+
+console.WriteLine(d.Hour); // 13
+
+-- Kind (DateTimeKind) --
+-- Tipo, local ou universal
+
+console.WriteLine(d.Kind);
+
+-- Milliseconds (Int) --
+
+console.WriteLine(d.Milisecond // 275
+
+-- Minute (Int) --
+
+console.WriteLine(d.Minute); // 45
+
+-- Month (Int) --
+
+console.WriteLine(d.Month); // 8
+
+-- Second (Int) --
+
+console.WriteLine(d.Second); // 58
+
+-- Ticks (Long) --
+
+console.WriteLine(d.Ticks); // 631334799582750000
+
+-- TimeOfDay (TimeSpan) --
+
+console.WriteLine(d.TimeOfDay); // Horario, Duração
+
+-- Year (Int) --
+
+console.WriteLine(d.Year); // 2000
+
+----- Formatações -----
+
+DateTime d = new DateTime(2000, 8, 12, 13, 45, 58);
+
+-- ToLongDateString() --
+Console.WriteLine(d.ToLongDateString()); // Data mais completa, converte para string
+
+-- ToLongTimeString() --
+Console.WriteLine(d.ToLongTimeString()); // Horario, Converte em String
+
+-- ToShortDateString() --
+Console.WriteLine(d.ToShortDateString()); // Data em formato Resumido
+
+-- ToShortTimeString() --
+Console.WriteLine(d.ToShortTimeString()); // Hora  minuto  
+  
+-- ToString() com mascara --  
+Console.WriteLine(d.ToString("yyyy/MM/dd HH:mm:ss"))  
+
+-- ToString() com mascara -- 
+Console.WriteLine(d.ToString("yyyy/MM/dd HH:mm:ss.fff"));
+
+----- Operações -----
+
+DateTime d = new DateTime(2000, 8, 12, 13, 45, 58);
+
+-- Add() --
+
+DateTime d2 = d.Add();
+
+-- AddDays() --
+
+DateTime d2 = d.AddDays(2);
+
+-- AddHours() --
+
+DateTime d2 = d.AddHours(2);
+
+-- AddMilliseconds() --
+
+DateTime d2 = d.AddMilliseconds(2);
+
+-- Addminutes() --
+
+DateTime d2 = d.AddMinutes(2);
+
+-- AddMonths() --
+
+DateTime d2 = d.AddMonths(2);
+
+-- AddSeconds() --
+
+DateTime d2 = d.AddSeconds(2);
+
+-- AddTicks() --
+
+DateTime d2 = d.AddTicks();
+
+-- AddYears() --
+
+DateTime d2 = d.AddYears(2);
+
+-- Subtract(timeSpan) -- 
+
+TimeSpan df = d.Subtract(OutraData); // Diferença de Datas
+
+-- Subtract(dateTime) --
+
+Datetime dt = d.Subtract(OutraData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- LINGUAGEM_DA_PROGRAMACAO/CSHARP/TOPICOS_ESPECIAIS/TimeSpan.cs
+++ LINGUAGEM_DA_PROGRAMACAO/CSHARP/TOPICOS_ESPECIAIS/TimeSpan.cs
@@ -1,2 +1,134 @@
 
 /* TIMESPAN */
+
+-- Representa uma duração, intervalo de instantes
+
+----- Instanciando -----
+
+TimeSpan t1 = new TimeSpan(hora, minuto, segundo);
+
+Ex.
+TimeSpan t1 = new TimeSpan(5, 1, 30);
+
+----- Instanciando Construtor -----
+
+TimeSpan t1 = new TimeSpan(); // Padrao, duração 0
+TimeSpan t2 = new TimeSpan(900000000L); // Ticks
+TimeSpan t3 = new TimeSpan(hora, minuto, segundo);
+TimeSpan t4 = new TimeSpan(dia, horas, minutos, segundos);
+TimeSpan t5 = new TimeSpan(dia, hora, minutos, segundos, milisegundos);
+
+----- Instanciando Usando FROM -----
+
+TimeSpan t1 = TimeSpan.FromDays(dia); // 1.5
+TimeSpan t2 = TimeSpan.FromHours(horas); // 1.5
+TimeSpan t3 = TimeSpan.FromMinutes(minutos); // 1.5
+TimeSpan t4 = TimeSpan.FromSeconds(segundos); // 1.5
+TimeSpan t5 = TimeSpan.FromMiliseconds(Milisegundos); // 1
+TimeSpan t6 = TimeSpan.FromTicks(Ticks); // 900000000L
+
+----- Propriedades -----
+
+
+TimeSpan t = new TimeSpan(2, 3, 5 ,7, 11);
+
+-- Days --
+
+Console.WriteLine(t.Days); // 2
+
+-- Hours --
+
+Console.WriteLine(t.Hours); // 3
+
+-- Minutes --
+
+Console.WriteLine(t.Minutes); // 5
+
+-- Milliseconds --
+
+Console.WriteLine(t.Milliseconds); // 11
+
+-- Seconds --
+
+Console.WriteLine(t.Seconds); // 7
+
+-- Ticks --
+
+Console.WriteLine(t.Ticks); // 1839070110000
+
+-- TotalDays --
+
+Console.WriteLine(t.TotalDays); // 2,128
+
+-- TotalHours --
+
+Console.WriteLine(t.TotalHours); // 51,082
+
+-- TotalMinutes --
+
+Console.WriteLine(t.TotalMinutes); // 3065,11685
+
+-- TotalSeconds --
+
+Console.WriteLine(t.TotalSeconds); // 183907,011
+
+-- TotalMilliseconds --
+
+Console.WriteLine(t.TotalMilliseconds); // 183907011
+
+-- Max Value --
+TimeSpan t1 = TimeSpan.MaxValue;
+
+-- Min Value --
+TimeSpan t1 = TimeSpan.MinValue;
+
+-- Zero --
+TimeSpan t1 = TimeSpan.Zero;
+
+----- Operações -----
+
+TimeSpan t = new TimeSpan(1, 30, 10);
+TimeSpan t1 = new TimeSpan(0, 10, 5);
+
+-- Add() --
+
+TimeSpan soma = t.Add(t1)
+Console.WriteLine(soma); 
+
+-- Subtract() --
+
+TimeSpan subtra = t.Subtract(t1)
+Console.WriteLine(subtra); 
+
+-- Multiply() --
+
+TimeSpan mult = t1.Multiply(2.0); // Recebe um double
+Console.WriteLine(mult); 
+
+-- Divide() --
+
+TimeSpan divide = t1.Divide(2.0); // Recebe um double
+Console.WriteLine(divide); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
