033d3e961dadec72246ca46ce946b06a967ae950
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-01-05 14:20:49 -0300

    Updates C#

--- LINGUAGEM_DA_PROGRAMACAO/CSHARP/HERANCA_e_POLIMORFISMO/Classes_e_Metodos-Selados.cs
+++ LINGUAGEM_DA_PROGRAMACAO/CSHARP/HERANCA_e_POLIMORFISMO/Classes_e_Metodos-Selados.cs
@@ -1,2 +1,17 @@
 
 /* CLASSES E MÉTODOS SELADOS */
+
+-- Palavra chave: ' sealed '
+
+-- Evita que a classe seja herdada 
+
+-- Evita que um método sobreposto possa ser sobreposto novamente
+
+sealed class selada(){
+	
+}
+
+
+public sealed override void nomeClasse(){
+	
+}
--- LINGUAGEM_DA_PROGRAMACAO/CSHARP/HERANCA_e_POLIMORFISMO/Heranca.cs
+++ LINGUAGEM_DA_PROGRAMACAO/CSHARP/HERANCA_e_POLIMORFISMO/Heranca.cs
@@ -1,2 +1,18 @@
 
 /* HERANÇA */
+
+-- Herdar dados de outra classe
+
+-- ':'
+
+public class Classe Que Esta : Classe Que Vai Herdar os Dados
+{
+	
+}
+
+// Aproveitando Construtor
+public class ClasseQueEsta(parametros) : base(Construtores para Reuso)
+{
+	
+}
+ 
