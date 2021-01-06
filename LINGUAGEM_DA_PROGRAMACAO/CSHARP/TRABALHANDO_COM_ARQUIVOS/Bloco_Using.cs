89b27f153f8a052d7c6859e0cb094b3cfd46eb95
Alexandre Tavano <73032368+alexandretavanocardoso@users.noreply.github.com>
2021-01-06 16:49:10 -0300

    File - FileInfo - StreamReader - FileStream - etc

--- LINGUAGEM_DA_PROGRAMACAO/CSHARP/TRABALHANDO_COM_ARQUIVOS/Bloco_Using.cs
+++ LINGUAGEM_DA_PROGRAMACAO/CSHARP/TRABALHANDO_COM_ARQUIVOS/Bloco_Using.cs
@@ -1,3 +1,39 @@
 
 
 /* BLOCO USING */
+
+-- Usado no lugar do "finally" para arquivos
+
+string path = "caminho";
+
+using (FileStream fs = new FileStream(path, ModoDeAbertura)){
+	
+}
+
+
+using (FileStream fs = new FileStream(path, ModoDeAbertura)){
+
+	using (StreamReader sr = new StreamReader(fs)){
+	
+	}
+	
+}
+
+-- Para tratar as exceções
+
+try 
+{
+	using (FileStream fs = new FileStream(path, ModoDeAbertura)){
+
+		using (StreamReader sr = new StreamReader(fs)){ 
+		
+		
+		}
+	
+	}
+	
+} catch (IOException) {
+	// mensagem
+}
+
+	
--- LINGUAGEM_DA_PROGRAMACAO/CSHARP/TRABALHANDO_COM_ARQUIVOS/Directory_DirectoryIndo.cs
+++ LINGUAGEM_DA_PROGRAMACAO/CSHARP/TRABALHANDO_COM_ARQUIVOS/Directory_DirectoryIndo.cs
@@ -1,3 +1,36 @@
 
 
 /* DIRECTORY - DIRECTORYINFO */
+
+-- Operações com pastas (create, enumerate, get files)
+
+-- Directory - static members
+
+-- DirectoryIndo - instance members
+
+/* Exemplo */
+
+string pah = "Caminho"
+
+
+try{
+	
+	// Listar Subpastas a partir de uma pasta original
+	IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); // tem algumas sobrecargas
+	foreach(string s in folders){
+		Console.WriteLine(s);
+	}
+	
+	// Listar Arquivos a partir de uma pasta original
+	IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories); // tem algumas sobrecargas
+	foreach(string s in files){
+		Console.WriteLine(s);
+	}
+	
+	// Criar Pasta
+	Directory.createDirectory("CaminhoCompletoDaNovaPasta");
+}
+catch (IOException e) {
+	Console.Write("Erro");
+	Console.Write(e.Message);
+}
