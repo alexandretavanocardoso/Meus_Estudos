e55290cf53dfa2f4a67078fe0d47947d177cda45
alexandretavanocardoso <AlexandreTavanoDeveloper@outlook.com>
2021-02-26 22:46:32 -0300

    Js - DOM, BOM, Estrutura de Repetição

--- JAVASCRIPT/BOM.js
+++ JAVASCRIPT/BOM.js
@@ -3,13 +3,58 @@
 ************/
 
 // #region [ Window ]
+// https://www.w3schools.com/js/js_window.asp
+var janela = document.getElementById("janela");
+
+// Fecha
+janela.close();
+
+// Abre
+janela.open();
+
+// Abre e consegue chamar uma url
+janela.open("http://google.com");
+
+// Abre com estilizacao
+janela.open("height=300px, widht=20px");
+
+// Print da tela
+window.print();
 // #endregion [ Window ]
 
 // #region [ Screen ]
+// https://www.w3schools.com/js/js_window_screen.asp
+
+// Altura e largura do Browser, excluido a barra de ferramentas
+window.screen.availHeight();
+window.screen.availWidth();
+
+// Altura e largura do Browser, com a barra de ferramentas
+window.screen.height();
+window.screen.width();
 // #endregion [ Screen ]
 
 // #region [ Location ]
+// https://www.w3schools.com/js/js_window_location.asp
+
+// Redirecionamento com href
+window.location.href = "http://google.com";
+
+// Recarrega a pagina
+window.reload();
+
 // #endregion [ Location ]
 
 // #region [ Timing ]
+// https://www.w3schools.com/js/js_timing.asp
+
+// Executa a ação UMA vez após o tempo
+setTimeout(function() {
+    alert("ola");
+}, 1000);
+
+// SEMPRE executar a acao após o tempo
+setInterval(function() {
+    alert("ola");
+}, 1000);
 // #endregion [ Timing ]
--- JAVASCRIPT/estruturaRepeticao.js
+++ JAVASCRIPT/estruturaRepeticao.js
@@ -3,16 +3,85 @@
 ************/
 
 // #region [ WHILE ]
+// Enquanto
+while(Condicao){
+
+    // código
+
+// #region [ OUTROS COMANDOS QUE PODEMOS COLOCAR ]
+    // parada Obrigatoria
+    break;
+
+    // Interrompe o bloco de codigo
+    // e pula pra condicao principal
+    continue;
+// #endregion [ OUTROS COMANDOS QUE PODEMOS COLOCAR ]
+
+    // critério de parada, pois incrementa
+    x++; // Incremento
+}
+// EX
+var x = 1;
+while(x <= 10){
+    document.write(x + '<br />');
+
+    // parada Obrigatoria
+    break;
+
+    // Interrompe o bloco de codigo
+    // e pula pra condicao principal
+    continue;
+
+    // critério de parada
+    x++; // Incremento
+}
 // #endregion [ WHILE ]
 
 // #region [ DO WHILE ]
+do {
+    // comando 
+    // passa primeiro no DO, depois pr o While
+} while (condicao) {
+
+    x++;
+}
 // #endregion [ DO WHILE ]
 
 // #region [ FOR ]
+for(variavel;condicao;incremento){
+    // comando
+}
+
+// Ex, Incremento
+for(var i = 0; i <= 10; i++){
+    document.write(i);
+}
+// Ex, Decremento
+for(var i = 10; i <= 0; i--){
+    document.write(i);
+}
 // #endregion [ FOR ]
 
 // #region [ FOR IN ]
+var listaConvidados = ["Ale", "Tav", "Maria"];
+
+for ( var convidados in listaConvidados ){
+    document.write(convidados + "=" + listaConvidados[convidados]);
+}
 // #endregion [ FOR IN ]
 
 // #region [ FOREACH ]
+// Aplicado a Arrays
+var lista = ["1", "2"];
+
+lista.forEach(function(valor, indice, array) {
+    document.write("Indice:" + indice);
+    document.write("Valor:" + valor);
+    document.write("Array:" + array);
+});
+
+lista.forEach(function(item) {
+    document.write("item:" + item);
+});
+
 // #endregion [ FOREACH ]
